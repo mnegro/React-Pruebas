a5eec40cff32d13424a45ede85d8873a9438f2ef
Maxi N <maximilianonegro22@gmail.com>
2021-10-26 17:47:08 -0300

    feat: P-U a deses-arr

---
+++ src/tests/base/07-deses-arr.test.js
@@ -0,0 +1,15 @@
+import { retornaArreglo } from "../../base/07-deses-arr"
+
+
+describe('Prubas en desenstructuracion',()=>{
+    test('debe de retornar un srein y numero', () => {
+        const [letras, numeros] = retornaArreglo();//['ABC, 123];
+        /* expect( arr ).toEqual(['ABC',123]); */
+        expect(letras).toBe('ABC');
+        expect( typeof letras ).toBe('string');
+
+        expect(numeros).toBe(123);
+        expect( typeof numeros ).toBe('number');
+    })
+    
+})
